eb4970de95d13190c10a572c5e305107aee61e5f
WolfSpiderman <wolfspiderman@yahoo.com>
2023-01-30 21:55:12 -0600

    update to html page

---
+++ html-page/assets/script.js
@@ -0,0 +1,9 @@
+
+("html-example.png").click(function(){
+	// $("#show_image_popup").fadeIn();
+	$("#show_image_popup").show()
+})
+(".close-btn").click(function(){
+	// $("#show_image_popup").fadeOut()
+	$("#show_image_popup").hide()
+})
